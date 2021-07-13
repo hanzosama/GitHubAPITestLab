9687c11db7ca6b5a9d919fab51b0a09ddd5eff75
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2021-07-13 09:27:11 -0500

    Update configuration

--- GitHubAPITestLab/Network/GitHubAPIRouter.swift
+++ GitHubAPITestLab/Network/GitHubAPIRouter.swift
@@ -9,50 +9,50 @@ import Alamofire
 import Foundation
 
 enum GitHubAPIRouter {
-    
+
     static let BASE_URL = "https://api.github.com"
-    static let CLIENT_ID = "raiman114@gmail.com"
-    static let API_KEY = "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"
+    static let CLIENT_ID = ""
+    static let API_KEY = ""
     static let APPLICATION_JSON = "application/json"
-    
-    
+
+
     case searchReposioty(queryParameter: [String:Any])
-    
+
     var method:HTTPMethod {
         switch self {
         case .searchReposioty:
             return .get
         }
     }
-    
+
     var path:String {
         switch self {
         case .searchReposioty:
             return "/search/repositories"
         }
     }
-    
+
     var parameters: Parameters?{
         switch self {
         case .searchReposioty(let queryParameter):
             return queryParameter
         }
     }
-    
+
     var accept: String {
         switch self {
         case .searchReposioty:
             return "application/json"
         }
     }
-    
+
     var consume: String {
         switch self {
         case .searchReposioty:
             return "application/vnd.github.v3+json"
         }
     }
-    
+
 }
 
 extension GitHubAPIRouter:  URLRequestConvertible {
@@ -62,7 +62,7 @@ extension GitHubAPIRouter:  URLRequestConvertible {
         request.httpMethod = method.rawValue
         request.setValue(accept, forHTTPHeaderField: "Accept")
         request.setValue(consume, forHTTPHeaderField: "Consume")
-        
+
         switch self {
         case .searchReposioty:
             request =  try URLEncoding.default.encode(request, with: parameters)
